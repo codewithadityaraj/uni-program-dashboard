63abcfe8bfd3d8639c27f0895b3345260dd8b78e
codewithadityaraj <adiirajsinghchouhan@gmail.com>
2026-06-04 16:27:50 +0530

    Code Changes

--- run.bat
+++ run.bat
@@ -5,4 +5,4 @@ if not exist ".venv" (
 )
 call .venv\Scripts\activate.bat
 pip install -q -r requirements.txt
-python -m uvicorn server:app --reload --host 127.0.0.1 --port 8000
+python -m uvicorn server:app --reload --host 127.0.0.1 --port 8005
